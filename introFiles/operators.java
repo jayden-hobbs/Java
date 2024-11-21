b3fdc8a5360524fe073bb1b26126fc2d9caa84f3
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-21 15:59:40 +0000

    Added opertors.java

---
+++ introFiles/operators.java
@@ -0,0 +1,18 @@
+public class operators {
+    public static void main(String[] args) {
+        System.out.println(2+2); // prints simple addition = 4
+        System.out.println(2-2); // prints simple subtraction = 0
+        System.out.println(3*2); // prints simple multiplication = 6
+        System.out.println(6/2); // prints simple division = 3
+        System.out.println(5%2); // prints the remainder of 5 divided by 2 = 1
+        System.out.println(5/2); // prints the quotient of 5 divided by 2 = 2
+
+        int number1 = 5;
+        System.out.println(++number1); // prints 6
+
+        int number2 = 5;
+        System.out.println(--number2); // prints 4
+
+
+    }
+}
