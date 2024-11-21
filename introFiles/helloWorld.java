be983dac39ec877d50e314ddab91372ecf52e5fe
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-21 12:38:06 +0000

    Capital S :(

--- introFiles/helloWorld.java
+++ introFiles/helloWorld.java
@@ -1,5 +1,5 @@
 public class helloWorld {
-    public static void helloWorld(String[] args) {
-        system.out.println("Hello World!")
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
