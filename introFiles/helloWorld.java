ecde9dd7f670c241e9977dc9f7d0aa592f8469c1
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-21 13:15:26 +0000

    Added comments to helloWorld.java

--- introFiles/helloWorld.java
+++ introFiles/helloWorld.java
@@ -1,5 +1,5 @@
-public class helloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class helloWorld {   // class name must match the file name
+    public static void main(String[] args) {  // main() is a method and any code within it will be executed. Every program must contain it
+        System.out.println("Hello World!"); // System is a built in feature. out is short for output and println() means print line
+    } // A ; is used to mark the begining and end of a block of code
 }
