fc36e2a315f41262e81e3698c3cfe131520e5e3d
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-21 15:17:31 +0000

    Added typeCasting.java

---
+++ introFiles/typeCasting.java
@@ -0,0 +1,12 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        int age = 18;
+        double newAge = age; // this is an example of widening type casting
+        System.out.println(newAge); // prints 18.0
+
+        double pi = 3.14;
+        int newPi = (int) pi; // this is an example of narrowing type casting
+        System.out.println(newPi); // prints 3
+
+    }
+}
