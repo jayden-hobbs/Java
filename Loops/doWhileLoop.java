5ca56d3d4c2a4732a95a90b5b34ad9c6258f5d31
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-21 22:30:27 +0000

    Added doWhileLoop.java

---
+++ Loops/doWhileLoop.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class doWhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5); // do-while loop will run at least once, then check the condition
+    }
+}
--- Loops/whileLoop.java
+++ Loops/whileLoop.java
@@ -3,9 +3,9 @@ package Loops;
 public class whileLoop {
     public static void main(String[] args) {
         int i = 0;
-        while (i < 5) {
+        while (i < 5) { // while loop will run until the condition is true
             System.out.println(i);
-            i++;
+            i++; // increment the value of i by 1
         }
     }
 }
