6a61ae5d453c6a3b39c0d9a5f87dd2700a8f7617
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-21 22:43:02 +0000

    Added comments

--- Loops/nestedLoop.java
+++ Loops/nestedLoop.java
@@ -2,10 +2,10 @@ package Loops;
 
 public class nestedLoop {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) { // outer loop
-            for (int j = 0; j < 3; j++) { // inner loop
-                System.out.println(i + " " + j);
+        for (int i = 0; i < 3; i++) { // this will run 3 times as the condition is i < 3
+            for (int j = 0; j < 3; j++) { // this will run 3 times each time the outer loop runs as the condition is j < 3
+                System.out.println(i + " " + j); // print the value of i and j together as a pair
             }
         }
-    }
-}
+    } 
+} 
