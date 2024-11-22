9c8b85dfb40d99ce38eb5415651c43f4445411ef
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-22 14:52:38 +0000

    Added returns.java

---
+++ javaMethods/returns.java
@@ -0,0 +1,10 @@
+package javaMethods;
+
+public class returns {
+    static int method1(int x) {
+        return 5 + x;
+    }
+    public static void main(String[] args) {
+        System.out.println(method1(3));
+    }
+}
