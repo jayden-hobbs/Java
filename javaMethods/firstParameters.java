0434124394be4db96f158edd232c3412d7c66a33
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-22 14:16:47 +0000

    Added firstParameters.java

---
+++ javaMethods/firstParameters.java
@@ -0,0 +1,12 @@
+package javaMethods;
+
+public class firstParameters {
+    static void method1(String firstName) {
+        System.out.println(firstName + " Smith");
+    }
+    public static void main(String[] args) {
+        method1("John");
+        method1("Jane");
+        method1("Tom");
+    }
+}
