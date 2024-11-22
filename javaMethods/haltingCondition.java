d15224afc46f316a8fca094112fd2f6b391633b3
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-22 15:58:53 +0000

    Added haltingCondition.java

---
+++ javaMethods/haltingCondition.java
@@ -0,0 +1,16 @@
+package javaMethods;
+
+public class haltingCondition {
+    public static void main(String[] args) {
+        int result = sum(5, 10);
+        System.out.println(result);
+
+    }
+    public static int sum(int start, int end) {
+        if (end > start) {
+            return end + sum(start, end - 1);
+        } else {
+            return end;
+        }
+    }
+}
