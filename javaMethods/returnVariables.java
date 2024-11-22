a1169e3ac7c5b11c2904bf9fed0a3adb3d86c9e8
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-11-22 14:54:34 +0000

    Added returnVariables

---
+++ javaMethods/returnVariables.java
@@ -0,0 +1,10 @@
+package javaMethods;
+
+public class returnVariables {
+    static int method1(int x, int y) {
+        return x + y;
+    }
+    public static void main(String[] args) {
+        System.out.println(method1(3, 5));
+    }
+}
