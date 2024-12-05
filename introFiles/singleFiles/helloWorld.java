c91cc2159d89b6561c356259f021523919a9df3c
Jayden Hobbs <j.hobbs3392@student.leedsbeckett.ac.uk>
2024-12-05 23:51:24 +0000

    Modified helloWorld.java

--- introFiles/singleFiles/helloWorld.java
+++ introFiles/singleFiles/helloWorld.java
@@ -1,6 +1,6 @@
 package introFiles.singleFiles;
 public class helloWorld {   // class name must match the file name
     public static void main(String[] args) {  // main() is a method and any code within it will be executed. Every program must contain it
-        System.out.println("Hello World!"); // System is a built in feature. out is short for output and println() means print line
+        System.out.println("Hello Amelia!"); // System is a built in feature. out is short for output and println() means print line
     } // A ; is used to mark the begining and end of a block of code
 }
